239ba1fe964b17c00a2bb3a889f725870ad92805
charosanchezinda-spec <charosanchezinda@gmail.com>
2026-07-13 17:01:47 -0300

    Add import for os module

--- imputacion.py
+++ imputacion.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import os
 from sklearn.linear_model import LogisticRegression, LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import (
