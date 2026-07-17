6da2100f2bcb225d0d848c26bca88e96a7c5fe7e
charosanchezinda-spec <charosanchezinda@gmail.com>
2026-07-17 00:51:14 -0300

    Fix formatting and whitespace in tracking.py

--- tracking.py
+++ tracking.py
@@ -1,19 +1,20 @@
 import numpy as np
-import pandas as pd
+import pandas as pd 
 def calcular_tracking(df, ventana_col, peso_col):
-  #Tracking imagen  
-  tracking_imagen = (
+    tracking_imagen = (
         df.groupby(ventana_col)
         .apply(lambda g: np.average(g['imagen_del_candidato'], weights=g[peso_col]))
         .reset_index(name='trackeo')
-    )
+    ) 
     # Tracking voto
     candidatos = df['voto'].unique().tolist()
     for c in candidatos:
         df[f'vota_{c}'] = (df['voto'] == c).astype(int)
+ 
     tracking_voto = (
         df.groupby(ventana_col)
-        .apply(lambda g: pd.Series({f"Vota_{c}": np.average(g[f'vota_{c}'], weights=g[peso_col]) * 100
+        .apply(lambda g: pd.Series({
+            f"Vota_{c}": np.average(g[f'vota_{c}'], weights=g[peso_col]) * 100
             for c in candidatos
         }))
         .reset_index()
